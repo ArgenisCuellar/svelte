df9468336b822caa2a7af49f65cd728dd6545dcd
Argenis Cuellar <argenis.cuellar01@gmail.com>
2024-01-13 23:58:47 -0600

    chore: workflow test

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <title>Test</title>
+    <meta name="description" content="Push Test" />
+</svelte:head>
+
+<div class="text-column">
+    <h1>This is a hello world from Svelte!</h1>
+</div>
